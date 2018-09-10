40ad27ac57647a150c2e1a8d0796d902f29c10a9
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-09-10 08:16:23 +0100

    update jenkinsfile

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "product" {
+  type    = "string"
+}
+
+variable "component" {
+  type    = "string"
+}
+
+variable "location" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+}
+
+variable "subscription" {
+  type = "string"
+}
+
+variable "capacity" {
+  default = "1"
+}
+
+variable "common_tags" {
+  type = "map"
+}
