a67dbec701b533324f64b240c8f59f744b51b8f5
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-09-12 13:14:53 +0100

    removed not needed locals

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-locals {
-  artifactsBaseUrl = "https://raw.githubusercontent.com/elastic/azure-marketplace/6.3.0/src"
-  templateUrl = "${local.artifactsBaseUrl}/mainTemplate.json"
-  elasticVnetName = "elastic-search-vnet"
-  elasticSubnetName = "elastic-search-subnet"
-  vNetLoadBalancerIp = "10.112.0.4"
-}
-
 module "elastic" {
   source = "git@github.com:hmcts/cnp-module-elk.git?ref=master"
   product = "${var.product}"
