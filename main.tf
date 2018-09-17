af51907b3740f2b49633a8ae998ccc963ef4a82f
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-09-17 11:59:40 +0100

    added population of vault with ElasticSearch url

--- main.tf
+++ main.tf
@@ -5,3 +5,26 @@ module "elastic" {
   env = "${var.env}"
   common_tags = "${var.common_tags}"
 }
+
+locals {
+  // Vault name
+  previewVaultName = "${var.product}-aat"
+  nonPreviewVaultName = "${var.product}-${var.env}"
+  vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
+
+  // Shared Resource Group
+  previewResourceGroup = "${var.product}-shared-aat"
+  nonPreviewResourceGroup = "${var.product}-shared-${var.env}"
+  sharedResourceGroup = "${(var.env == "preview" || var.env == "spreview") ? local.previewResourceGroup : local.nonPreviewResourceGroup}"
+}
+
+data "azurerm_key_vault" "ccd_shared_key_vault" {
+  name = "${local.vaultName}"
+  resource_group_name = "${local.sharedResourceGroup}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name = "${var.product}-ELASTIC_SEARCH_URL"
+  value = "${module.elastic.loadbalancerManual}"
+  vault_uri = "${data.azurerm_key_vault.ccd_shared_key_vault.vault_uri}"
+}
