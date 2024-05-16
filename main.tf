1f95d2efe3985d29244f0eb66fc97e5dcb70501e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-05-16 15:54:01 +0100

    update branch

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "elastic" {
 
 module "elastic2" {
   count                         = var.env == "aat" ? 1 : 0
-  source                        = "git@github.com:hmcts/cnp-module-elk.git?ref=RDM-13038-ek"
+  source                        = "git@github.com:hmcts/cnp-module-elk.git?ref=DTSPO-17635/datadisk-sku"
   vmHostNamePrefix              = "ccd-dtspo-17635-"
   product                       = "${var.raw_product}-ccd-dtspo-17635"
   location                      = var.location
