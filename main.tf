7089d1ce0b08be4e9f8d71a6a08031410aab6fc0
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-10-03 10:59:00 +0100

    Add conditional to data source

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ data "azurerm_monitor_data_collection_rule" "linux_data_collection_rule" {
 }
 
 data "azurerm_virtual_machine" "elk_vms" {
-  for_each = toset(var.vm_names)
+  for_each = var.env != "production" ? toset(var.vm_names) : toset([])
 
   name                = each.value
   resource_group_name = "ccd-elastic-search-${var.env}"
