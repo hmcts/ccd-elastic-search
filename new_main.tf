aeedde12d7705c51214bec651c162b56bd2524ef
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-04-23 15:06:48 +0100

    added terraform_data resource

--- new_main.tf
+++ new_main.tf
@@ -86,22 +86,25 @@ resource "azurerm_key_vault_secret" "admin_ssh_key" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
-import {
-  to = module.elastic2["ccd-data-2"].module.virtual-machines.azurerm_virtual_machine_data_disk_attachment.data_disk_attachments["disk1"]
-  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/ccd-elastic-search-sandbox/providers/Microsoft.Compute/virtualMachines/ccd-data-2/dataDisks/ccd-data-2-datadisk1"
+resource "azurerm_key_vault_secret" "admin_name" {
+  name         = "ccd-vm-admin-name"
+  value        = "ccdadmin"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
-import {
-  to = module.elastic2["ccd-data-1"].module.virtual-machines.azurerm_virtual_machine_data_disk_attachment.data_disk_attachments["disk1"]
-  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/ccd-elastic-search-sandbox/providers/Microsoft.Compute/virtualMachines/ccd-data-1/dataDisks/ccd-data-1-datadisk1"
-}
+resource "terraform_data" "vm" {
+  for_each = var.env == "sandbox" ? var.vms : {}
+  connection {
+    type        = "ssh"
+    host        = each.value.ip
+    user        = azurerm_key_vault_secret.admin_name.value
+    private_key = azurerm_key_vault_secret.admin_ssh_key.value
+    timeout     = "15m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello, World!'",
+    ]
+  }
 
-import {
-  to = module.elastic2["ccd-data-3"].module.virtual-machines.azurerm_virtual_machine_data_disk_attachment.data_disk_attachments["disk1"]
-  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/ccd-elastic-search-sandbox/providers/Microsoft.Compute/virtualMachines/ccd-data-3/dataDisks/ccd-data-3-datadisk1"
-}
-
-import {
-  to = module.elastic2["ccd-data-0"].module.virtual-machines.azurerm_virtual_machine_data_disk_attachment.data_disk_attachments["disk2"]
-  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/ccd-elastic-search-sandbox/providers/Microsoft.Compute/virtualMachines/ccd-data-0/dataDisks/ccd-data-0-datadisk2"
 }
