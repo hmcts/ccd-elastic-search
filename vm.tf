691372343b92f69256274b397fd1edd836639d72
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2025-05-07 08:16:59 +0100

    Seperate the VM and LB out

--- vm.tf
+++ vm.tf
@@ -1,3 +1,12 @@
+module "elastic_lb" {
+  source               = "github.com/hmcts/ccd-module-elastic-search.git?ref=DTSPO-25523-lb-1"
+  subnet_id            = data.azurerm_subnet.elastic_subnet.id
+  private_ip_address   = var.lb_private_ip_address
+  vms                  = var.vms
+  env                  = var.env
+  backend_vm_addresses = { for k, v in var.vms : k => v.ip }
+}
+
 module "elastic2" {
   for_each = var.env == "sandbox" ? var.vms : {}
 
@@ -19,9 +28,6 @@ module "elastic2" {
   managed_disks        = each.value.managed_disks
   soc_vault_name       = var.soc_vault_name
   soc_vault_rg         = var.soc_vault_rg
-  backend_vm_addresses = { for k, v in var.vms : k => v.ip }
-  vms                  = var.vms
-  private_ip_address   = var.lb_private_ip_address
 }
 
 
