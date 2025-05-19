029e7bb08a4a00cddb2205e48d15227f4f4e884d
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-05-19 10:16:37 +0100

    fixing import issues

---
+++ import_sbox.tf
@@ -0,0 +1,19 @@
+import {
+  to = azurerm_key_vault_secret.elastic_search_url_key_setting
+  id = "https://ccd-sandbox.vault.azure.net/secrets/ccd-ELASTIC-SEARCH-URL/ff4f0da523a34cacab5aeb78a38772c2"
+}
+
+import {
+  to = azurerm_key_vault_secret.es_data_nodes_url
+  id = "https://ccd-sandbox.vault.azure.net/secrets/ccd-ELASTIC-SEARCH-DATA-NODES-URL/1ab689731b9e4e7da06ab9ce9c0c5d6a"
+}
+
+import {
+  to = azurerm_key_vault_secret.admin_name
+  id = "https://ccd-sandbox.vault.azure.net/secrets/ccd-vm-admin-name/86ec94f004794a3fa94ec20b5d572848"
+}
+
+import {
+  to = azurerm_key_vault_secret.password
+  id = "https://ccd-sandbox.vault.azure.net/secrets/ccd-vm-admin-password/b0a81ee0f879474aab47939e83bbb42a"
+}
