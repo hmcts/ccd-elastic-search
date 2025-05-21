6b16243146727c739689de911fc8f2c200adaacd
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-05-21 09:40:56 +0100

    clean up terraform data resource

--- locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  defaults_main_hash = md5(file("./ansible/roles/elasticsearch/defaults/main.yml"))
-  task_install_hash  = md5(file("./ansible/roles/elasticsearch/tasks/1_install.yml"))
-  disk_mount_hash    = md5(file("./ansible/diskmount.yml"))
-  config_template    = md5(file("./ansible/elasticsearch.yml.j2"))
-}
