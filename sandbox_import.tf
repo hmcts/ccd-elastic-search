48c18309421c3945395bafedcd0505229890cd30
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-05-21 16:47:27 +0100

    apply import blocks from the copilot

--- sandbox_import.tf
+++ sandbox_import.tf
@@ -51,3 +51,63 @@ import {
   id       = "https://ccd-${each.key}.vault.azure.net/secrets/ccd-vm-ssh-private-key/363d2e9a4aa34b56baa3cd4522d0e588"
   to       = azurerm_key_vault_secret.ssh_private_key
 }
+
+
+
+# ...existing code...
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/loadBalancers/ccd-internal-${each.key}-lb/backendAddressPools/LBBE"
+  to       = azurerm_lb_backend_address_pool.this["${each.key}"]
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/loadBalancers/ccd-internal-${each.key}-lb/probes/es-probe-internal-http"
+  to       = azurerm_lb_probe.this["http"]
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/loadBalancers/ccd-internal-${each.key}-lb/probes/es-probe-transport-internal"
+  to       = azurerm_lb_probe.this["transport"]
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/applicationSecurityGroups/ccd-data-asg"
+  to       = azurerm_application_security_group.this
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/networkSecurityGroups/ccd-cluster-nsg"
+  to       = azurerm_network_security_group.nsg_group
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/networkInterfaces/ccd-data-0-nic"
+  to       = module.elastic2["ccd-data-0"].module.virtual-machines.azurerm_network_interface.vm_nic
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/networkInterfaces/ccd-data-1-nic"
+  to       = module.elastic2["ccd-data-1"].module.virtual-machines.azurerm_network_interface.vm_nic
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/networkInterfaces/ccd-data-2-nic"
+  to       = module.elastic2["ccd-data-2"].module.virtual-machines.azurerm_network_interface.vm_nic
+}
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Network/networkInterfaces/ccd-data-3-nic"
+  to       = module.elastic2["ccd-data-3"].module.virtual-machines.azurerm_network_interface.vm_nic
+}
+
+# ...existing code...
