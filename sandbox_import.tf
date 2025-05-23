2cfa5f3a8737b128bc3cea322e21510664dae615
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-05-23 12:31:27 +0100

    last attempt for today

--- sandbox_import.tf
+++ sandbox_import.tf
@@ -306,6 +306,14 @@ import {
 
 import {
   for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
+  id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Compute/virtualMachines/ccd-data-1/extensions/AADSSHLoginForLinux"
+  to       = module.elastic2["ccd-data-1"].module.virtual-machines.azurerm_virtual_machine_extension.entra[0]
+}
+
+
+
+import {
+  for_each = { for k, v in local.env_subs : k => v if k == "sandbox" }
   id       = "/subscriptions/${each.value}/resourceGroups/ccd-elastic-search-${each.key}/providers/Microsoft.Compute/virtualMachines/ccd-data-0/providers/Microsoft.Insights/dataCollectionRuleAssociations/vm-ccd-data-0-dcra"
   to       = module.elastic2["ccd-data-0"].module.virtual-machines.module.vm-bootstrap[0].azurerm_monitor_data_collection_rule_association.linux_vm_dcra[0]
 }
