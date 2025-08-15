7605b1da127ba0b55b19010533720439b3a1fb67
markdathornehmcts <mark.dathorne@hmcts.net>
2025-08-15 11:40:09 +0100

    CCD-5849 bump ES minor version to 9.0.4

--- variables.tf
+++ variables.tf
@@ -47,7 +47,7 @@ variable "vmSizeAllNodes" {
 variable "esVersion" {
   description = "ES version"
   type        = string
-  default     = "9.0.1"
+  default     = "9.0.4"
 }
 
 variable "storageAccountType" {
