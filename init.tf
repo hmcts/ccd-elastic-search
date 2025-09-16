b6a6ac456c50e091b47a1cf984f4148c9c9cae4d
Connor O'Kane <connor.o'kane@justice.gov.uk>
2025-09-16 16:53:18 +0100

    Adding Perftest import config

--- init.tf
+++ init.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  # subscription_id = "7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c"
+  subscription_id = "7a4e3bd5-ae3a-4d0c-b441-2188fee3ff1c"
 }
 
 provider "azurerm" {
