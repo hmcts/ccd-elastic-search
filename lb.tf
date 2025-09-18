38ec1baa8ab22b5d02e3830b5c054e9dacfda9d1
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2025-09-18 15:39:26 +0100

    Prod import

--- lb.tf
+++ lb.tf
@@ -1,5 +1,5 @@
 locals {
-  enabled_lb_envs = ["sandbox", "demo", "ithc", "aat"]
+  enabled_lb_envs = ["sandbox", "demo", "ithc", "aat", "prod"]
   is_enabled_env  = contains(local.enabled_lb_envs, var.env)
   env_map         = local.is_enabled_env ? { (var.env) = true } : {}
 
