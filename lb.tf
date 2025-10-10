2a3e2c3f7a3de8b4f7733d46fed4c90d0e23bca9
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2025-10-10 15:56:35 +0100

    Clean up lb and add moved state move blocks

--- lb.tf
+++ lb.tf
@@ -1,8 +1,4 @@
 locals {
-  enabled_lb_envs = ["sandbox", "demo", "ithc", "aat", "perftest", "prod"]
-  is_enabled_env = contains(local.enabled_lb_envs, var.env)
-  env_map        = local.is_enabled_env ? { (var.env) = true } : {}
-
   lb_ports = {
     "http" = {
       name       = "es-http-internal"
@@ -19,8 +15,6 @@ locals {
 }
 
 resource "azurerm_lb" "this" {
-  for_each = local.env_map
-
   name                = "ccd-internal-${var.env}-lb"
   location            = var.location
   resource_group_name = "ccd-elastic-search-${var.env}"
@@ -40,41 +34,40 @@ resource "azurerm_lb" "this" {
 }
 
 resource "azurerm_lb_backend_address_pool" "this" {
-  for_each        = local.env_map
   name            = "LBBE"
-  loadbalancer_id = azurerm_lb.this[each.key].id
+  loadbalancer_id = azurerm_lb.this.id
 }
 
 resource "azurerm_lb_backend_address_pool_address" "elastic_vm" {
-  for_each                = local.is_enabled_env ? var.vms : {}
+  for_each                = var.vms
   name                    = each.key
-  backend_address_pool_id = azurerm_lb_backend_address_pool.this[var.env].id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.this.id
   ip_address              = each.value.ip
   virtual_network_id      = data.azurerm_virtual_network.core_infra_vnet.id
 }
 
 resource "azurerm_lb_probe" "this" {
 
-  for_each            = local.is_enabled_env ? local.lb_ports : {}
+  for_each            = local.lb_ports
   name                = each.value.probe_name
   protocol            = "Tcp"
   port                = each.value.port
-  loadbalancer_id     = azurerm_lb.this[var.env].id
+  loadbalancer_id     = azurerm_lb.this.id
   interval_in_seconds = 5
   number_of_probes    = 2
 
 }
 
 resource "azurerm_lb_rule" "this" {
-  for_each                       = local.is_enabled_env ? local.lb_ports : {}
+  for_each                       = local.lb_ports
   name                           = each.value.name
   protocol                       = "Tcp"
   frontend_port                  = each.value.port
   backend_port                   = each.value.port
   frontend_ip_configuration_name = "LBFE"
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.this[var.env].id]
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.this.id]
   probe_id                       = azurerm_lb_probe.this[each.key].id
-  loadbalancer_id                = azurerm_lb.this[var.env].id
+  loadbalancer_id                = azurerm_lb.this.id
 
   disable_outbound_snat = true
 }
