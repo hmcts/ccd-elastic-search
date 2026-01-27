50b5cf0e59438206b739df4272f7f2084e1b1d06
Ben Neill <benjaminrneill@gmail.com>
2026-01-27 14:06:56 +0000

    Update init.tf

--- init.tf
+++ init.tf
@@ -9,8 +9,13 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
+
 
 provider "azurerm" {
   alias           = "aks-infra"
