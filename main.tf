8b88370f18bbc06576144ddae8baec5ce745bfeb
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-02-24 18:35:48 +0000

    Add storage container

--- main.tf
+++ main.tf
@@ -108,5 +108,9 @@ module "storageaccount" {
   resource_group_name  = var.resource_group_name
   location             = var.location
   account_kind         = var.account_kind
+  containers           = [{
+    name        = "es-snapshots"
+    access_type = "private"
+  }]
   common_tags          = var.common_tags
 }
