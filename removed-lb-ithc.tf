3d4d05ff0a778303a63237078f63735fbc11cf7b
Ben Neill <benjamin.neill@hmcts.net>
2026-03-31 12:10:05 +0000

    Drop temporary ITHC load balancer removed block

--- removed-lb-ithc.tf
+++
@@ -1,7 +0,0 @@
-# Temporary removed block for ITHC load balancer state cleanup.
-# This is intended to stop Terraform refreshing missing LB resources that still exist in state.
-# Remove this file when re-enabling LB creation/management for ITHC.
-
-removed {
-  from = module.load_balancers
-}
